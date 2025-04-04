9bae8d9bda0e404e338025277ad9d0627a6da255
Leonid <leonidledn@gmail.com>
2025-04-04 13:27:43 -0500

    ignore .vscode, edit main tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,27 +5,28 @@ import Constants from 'expo-constants';
 export default class App extends Component {
   render() {
     return (
-     null
-    )
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Change code in the editor and watch it change on your phone!
+        </Text>
+      </View>
+    );
   }
 }
 
-
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
   },
 });
